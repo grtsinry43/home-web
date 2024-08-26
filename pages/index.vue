4c1fde05d300e605a5521fa9bdba850344c51fd1
grtsinry43 <grtsinry43@outlook.com>
2024-08-26 17:23:38 +0800

    feat: 初始化页面布局

---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <UContainer>
+      <NuxtImg src="~/assets/logo.svg" alt="Nuxt.js Logo" width="50" height="50"/>
+    </UContainer>
+    <UContainer class="text-red-600">
+      <h1>Nuxt Routing set up successfully!</h1>
+      <p>Current route: {{ route.path }}</p>
+      <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    </UContainer>
+    <div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consequatur cum deleniti dolorem eos ex fugiat,
+        fugit, hic iste iure mollitia quisquam ratione repellendus saepe tempore. Accusantium consectetur hic sint.
+      </div>
+      <div>Accusantium autem deserunt dolorum facere laborum necessitatibus, perspiciatis placeat quis repellendus sint
+        tempora voluptates. Assumenda blanditiis debitis deleniti esse illo impedit magnam minus molestias, non
+        perspiciatis quia sed, suscipit, veritatis.
+      </div>
+      <div>Aperiam culpa, dolores, doloribus enim, illum incidunt ipsam natus necessitatibus nobis nostrum odio omnis
+        praesentium quo soluta veritatis. Alias debitis excepturi magni ut? Alias esse itaque iusto magnam, nobis unde.
+      </div>
+      <div>Architecto eligendi labore laborum veniam. Animi consequatur, dolor ea eius error est ex ipsam maiores minima
+        neque nihil nisi non nostrum obcaecati odio, omnis perferendis quae repudiandae rerum tempore vitae?
+      </div>
+      <div>Cumque earum eligendi laboriosam numquam, perspiciatis quae quibusdam. Eum, sed tempora. Aperiam doloribus
+        ipsa ipsum laboriosam mollitia ratione rem? Autem fugit itaque magni minus quae quia quidem rem reprehenderit
+        voluptate.
+      </div>
+      <div>Accusamus aliquid cumque cupiditate dolorum incidunt inventore maiores, necessitatibus omnis pariatur, quam
+        quisquam quo rerum tenetur. At earum ex harum impedit ipsam laudantium nisi odit quo saepe, sit unde vero?
+      </div>
+      <div>Eos, est, ut. Assumenda consequatur dolor eius excepturi facere incidunt labore magnam nobis pariatur
+        praesentium provident, quaerat sequi sunt totam voluptates! Aspernatur assumenda maxime mollitia officia
+        perferendis quaerat ratione repudiandae?
+      </div>
+      <div>Animi assumenda aut autem, consectetur consequuntur culpa cumque delectus doloremque dolores doloribus eius
+        error exercitationem in ipsum iusto magni nulla odio omnis perferendis possimus quaerat rem reprehenderit sed
+        voluptatibus voluptatum?
+      </div>
+      <div>Aperiam architecto consequuntur cum cupiditate, ducimus earum eligendi est facilis fuga harum hic impedit
+        ipsum iusto minima modi molestias neque nobis optio perferendis quia rem rerum saepe tenetur velit voluptate!
+      </div>
+      <div>Alias atque dolorem dolores eius esse itaque magnam nemo nulla odio optio pariatur quaerat quas qui quia,
+        quibusdam quisquam sapiente sed tempore ullam veniam. Atque hic magni nemo nulla placeat.
+      </div>
+      <div>Aperiam commodi eum facere harum, inventore quis repellendus sunt. Ad blanditiis commodi laboriosam minima
+        nobis possimus tempore tenetur veritatis. Blanditiis deleniti dicta iusto perferendis veritatis. Dolores iusto
+        nesciunt quis vero?
+      </div>
+      <div>Commodi, impedit in ipsum odit quaerat ratione! Ad amet architecto mollitia natus necessitatibus optio quam
+        sint, unde veniam. Assumenda eligendi eos labore minima nobis sapiente tenetur ullam. Aliquid, eos vel?
+      </div>
+      <div>Architecto autem blanditiis enim esse excepturi ipsam magnam molestias nisi odio placeat quia quo repellendus
+        similique, sit tempora veritatis voluptate. Corporis deleniti eaque in labore placeat praesentium reiciendis sit
+        voluptate?
+      </div>
+      <div>Amet aperiam consequatur culpa cumque deleniti dolorum expedita in laudantium nemo numquam pariatur quae, qui
+        quod veniam voluptatem! A deleniti doloribus, inventore magnam numquam officiis placeat quae rerum sit
+        voluptatem.
+      </div>
+      <div>Autem consequatur delectus, eaque laborum necessitatibus perspiciatis. Aliquid at consectetur, doloremque
+        dolorum eius, esse fuga ipsam laboriosam magni minus modi nemo odio odit perferendis quaerat quod reiciendis
+        soluta tempora vitae.
+      </div>
+      <div>Accusantium aliquam asperiores deleniti fuga illum impedit obcaecati quod! Architecto atque consectetur
+        incidunt, ipsam laborum libero maxime optio quisquam quos ratione. In iure libero modi, pariatur qui soluta
+        temporibus! Fugit?
+      </div>
+      <div>Alias deserunt dicta doloremque eius ex labore laborum similique tempora tenetur ullam? Accusantium aliquid
+        architecto beatae corporis ea expedita in ipsa neque nihil nostrum numquam odio, sequi tenetur ullam vitae!
+      </div>
+      <div>Adipisci architecto corporis facere, inventore magnam magni minus non quaerat reiciendis sunt veniam
+        voluptates! Alias commodi eaque eveniet exercitationem harum laudantium praesentium quae vel! Deserunt esse ex
+        officiis rem voluptatem!
+      </div>
+      <div>Cum cupiditate delectus distinctio dolore est id possimus praesentium rem repellendus veritatis. Delectus
+        distinctio, dolorum earum eveniet fugit ipsam minima temporibus. Aspernatur beatae consequatur deserunt
+        necessitatibus. Architecto eos labore magnam.
+      </div>
+      <div>Adipisci aperiam consequatur distinctio eligendi, eum iste modi mollitia nesciunt optio quae quasi qui quia
+        quisquam rem repellat rerum saepe sit suscipit ut voluptatem. Ea esse fuga in quisquam totam!
+      </div>
+    </div>
+  </div>
+</template>
